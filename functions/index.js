afae2a56c9a6498ae61a5e58bf828c7138a12882
GabreilDeml <gabrielddeml@gmail.com>
2020-03-20 14:02:39 -0400

    added a person

--- functions/index.js
+++ functions/index.js
@@ -23,27 +23,47 @@ app.intent('make_name', (conv, {color, number}) => {
         `I hope you like it. See you next time.`);
 });
 
-let dinner = ['one', 'two', 'three', 'four', 'five']
+let dinner = ['burgers', 'enchiladas', 'yakisoba', 'american chop suey', 'shepherds pie', 'swedish meatballs', 'salsa chicken', 'spaghetti',
+    'chinese chicken',
+    'pork pie',
+    'pizza',
+    'steak with peppers and onions',
+    'bratz',
+    'fettuccine alfredo',
+    'breaded chicken',
+    'buffalo chicken',
+    'meatloaf',
+    'barbeque pork',
+    'thai noodles',
+    'bagels with lox and cream cheese',
+    'fish tacos',
+    'tacos'];
 app.intent('what_is_for_dinner', (conv) => {
     let n = dinner[Math.floor(Math.random() * dinner.length)];
     conv.close(`You should have ` + n);
 });
 
-let people = ['Gabriel', 'Elizabeth', 'Andrew', 'Amy', 'Lillia'];
 app.intent('who_does_dishes', (conv) => {
     let chosen = getPeople();
     conv.close(chosen[0] + ` washes. ` + chosen[1] + ` Dries. ` + chosen[2] + ` does trash.`);
 });
 
-
+/**
+ * Generates a list of people that should do the jobs
+ * @return {[p1,p2,p3]}
+ */
 function getPeople() {
-    let peopleTmp = ['Gabriel', 'Elizabeth', 'Andrew', 'Amy', 'Lillia'];
+    let peopleTmp = ['Gabriel', 'Elizabeth', 'Andrew', 'Amy', 'Lillia', 'Joshua'];
     let peopleReturn = [];
     let i;
     for (i = 0; i < 3; i++) {
         let p = Math.floor(Math.random() * peopleTmp.length);
-        peopleReturn.push(peopleTmp[p]);
-        peopleTmp.splice(p, 1);
+        if (i != 2 && peopleTmp[p] == 'Joshua') {
+            i--;
+        } else {
+            peopleReturn.push(peopleTmp[p]);
+            peopleTmp.splice(p, 1);
+        }
     }
     return peopleReturn;
 }
